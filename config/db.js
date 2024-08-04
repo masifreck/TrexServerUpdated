c08ec76acc23c748b246ac87a614aacc64d26518
Mohd Asif <masifreck>
2024-08-04 22:47:22 +0530

    changes in database connection

--- config/db.js
+++ config/db.js
@@ -1,10 +1,11 @@
-const mysql=require('mysql2/promise')
+const mysql = require('mysql2/promise');
 
-const mysqlPool=mysql.createPool({
-    host:'localhost',
-    user:'root',
-    password:'root',
-    database:'trex'
-})
+const mysqlPool = mysql.createPool({
+    host: 'mysql-3d845bb1-masif854380-f879.d.aivencloud.com',
+    user: 'avnadmin',
+    password: 'AVNS_oGWsXc87Z1-DuqIMGGE',
+    port: 13683,
+    database: 'trex'
+});
 
-module.exports=mysqlPool
+module.exports = mysqlPool;
