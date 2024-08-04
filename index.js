ab4f19f4010e8d52155100d3df604df022f6480b
masifreck <97328838+masifreck@users.noreply.github.com>
2024-08-04 23:53:29 +0530

    Update index.js

--- index.js
+++ index.js
@@ -25,7 +25,7 @@ app.use('/api/v1/vendor',VendorRouter)
 app.use('/api',logoutRoutes)
 app.use('/api/v1/otpless', otplessRouter);
 app.get('/',(req,res)=>{
-    res.status(200).send('<h1>Trex Application</h1>')
+    res.status(200).send('<h1>Trex Application Server Running successfull</h1>')
 })
 
 //port
